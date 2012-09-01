065e56f2f2ee31dfda92899ae8e15d1436505c42
viras <viras@users.sourceforge.net>
2012-09-01 14:01:34 +0200

    fetching of organisations for dynamic tree now works

--- protected/controllers/JSONOrganisationController.php
+++ protected/controllers/JSONOrganisationController.php
@@ -1,14 +1,40 @@
 <?php
 class JSONOrganisationController extends Controller {
     public function japiGetChildren($organisation_id) {
-        return array( array(
-            "data" => array(
-                "title" => "JSON Node",
-                "icon" => "folder",
-                "attr" => array( "data-organisation-id" => 0 )
-            ),
-            "state" => "closed"
-        ) );
+        $result = array();
+        $model_childOrganisations = array();
+        
+        // find fitting organisation
+        if( $organisation_id ) {
+            $model_organisation = Organisation::model()->findByPk($organisation_id);
+            if( $model_organisation ) {
+                $model_childOrganisations = $model_organisation->organisations;
+            }
+        }
+        else {
+            $model_childOrganisations = Organisation::model()->findAll('parent_organisation_id IS NULL');
+        }
+        
+        if( $model_childOrganisations && count($model_childOrganisations) > 0 ) {
+            foreach( $model_childOrganisations as $model_childOrganisation ) {
+                // fetch data for display from organisation
+                $entry = array(
+                    "data" => array(
+                        "title" => $model_childOrganisation->description,
+                        "attr" => array( "data-organisation-id" => $model_childOrganisation->id )
+                    )
+                );
+                
+                // check if we have more child-organisations, if yes add closed state
+                if( count($model_childOrganisation->organisations) > 0 ) {
+                    $entry["state"] = "closed";
+                }
+                
+                $result[] = $entry;
+            }
+        }
+        
+        return $result;
     }
     
     public function actions() {
