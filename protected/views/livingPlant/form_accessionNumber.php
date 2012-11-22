75bfc5a4946fa1ead1bac12fc9a9b75db1cd8430
viras <viras@users.sourceforge.net>
2012-11-22 21:06:19 +0100

    accession-number only editable during creation of record

--- protected/views/livingPlant/form_accessionNumber.php
+++ protected/views/livingPlant/form_accessionNumber.php
@@ -1,13 +1,12 @@
+    <?php if($model_livingPlant->isNewRecord) { ?>
     <?php echo $form->labelEx($model_livingPlant, 'accession_number_id'); ?>
     <?php echo $form->textField($model_accessionNumber, 'year', array( 'size' => 4 )); ?>
-    <?php
-    if($model_accessionNumber->id) {
-        printf( '%05d', $model_accessionNumber->id );
+    -auto-
+    <?php echo $form->textField($model_accessionNumber, 'individual', array( 'size' => 3 )); ?>
+    <?php    
     }
     else {
     ?>
-    -auto-
-    <?php
-    }
-    ?>
-    <?php echo $form->textField($model_accessionNumber, 'individual', array( 'size' => 3 )); ?>
+    <?php echo $form->labelEx($model_livingPlant, 'accession_number_id'); ?>
+    <?php echo CHtml::encode($model_accessionNumber->accessionNumber); ?>
+    <?php } ?>
