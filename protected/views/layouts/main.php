df2c190b9b1aee8095b40dbad0298c881143a56c
wkollernhm <wolfgang.koller@nhm-wien.ac.at>
2013-11-15 09:18:31 +0100

    adding generic messagebox helper functions for use with jquery-ui dialogs

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -33,9 +33,27 @@
 
          <!-- custom styles -->
         <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/custom.css" />
+        
+         <!-- MsgBox -->
+         <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/msgbox.js" ></script>
     </head>
 
     <body>
+        <!-- generic ui-dialog which can be used to display errors -->
+        <div id="error-dialog" title="Error" style="display:none;">
+            <p>
+                <span class="ui-icon ui-icon-alert" style="float: left; margin: 0 7px 20px 0;"></span>
+                <span id="error-dialog_text"></span>
+            </p>
+        </div>
+        <!-- generic ui-dialog which can be used to display infos -->
+        <div id="info-dialog" title="Info" style="display:none;">
+            <p>
+                <span class="ui-icon ui-icon-info" style="float: left; margin: 0 7px 20px 0;"></span>
+                <span id="info-dialog_text"></span>
+            </p>
+        </div>
+
         <div class="container" id="page">
             <img id="logo" src="images/jacq_logo.png" width="120" height="60" />
 
