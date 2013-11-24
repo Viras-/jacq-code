7e0a70314ae4e708037a5fc8f29d44317aedff51
wkollernhm <wolfgang.koller@nhm-wien.ac.at>
2013-11-24 08:50:36 +0100

    leveraging Authorization component to generic check functions

--- protected/components/Authorization.php
+++ protected/components/Authorization.php
@@ -16,19 +16,49 @@ class Authorization extends CComponent {
      * @return boolean|NULL true / false or NULL if no explicit setting is given
      */
     public function botanicalObjectAccess($botanical_object_id, $user_id) {
+        return $this->checkAccess($user_id, AccessBotanicalObject::model(), $botanical_object_id, "botanical_object_id");
+    }
+    
+    /**
+     * Checks for group access on botanical object level
+     * @param string $group Name of group to check for
+     * @param int $botanical_object_id Botanical Object to check access for
+     * @return integer|string empty string if no result found, 1 on granted and 0 and denied
+     */
+    public function botanicalObjectAccessGroup($group, $botanical_object_id) {
+        return $this->checkAccessByType(AccessBotanicalObject::model(), $botanical_object_id, "botanical_object_id", $group, "group");
+    }
+    
+    /**
+     * Check for user access on botanical object level
+     * @param int $user_id ID of user to check for
+     * @param int $botanical_object_id ID of botanical object to check
+     * @return integer|string empty string if no result found, 1 on granted and 0 and denied
+     */
+    public function botanicalObjectAccessUser($user_id, $botanical_object_id) {
+        return $this->checkAccessByType(AccessBotanicalObject::model(), $botanical_object_id, "botanical_object_id", $user_id, "user");
+    }
+    
+    /**
+     * Check access to botanical object
+     * @param int $botanical_object_id ID of botanical object to check
+     * @param int $user_id ID of user for access checking (groups are fetched automatically)
+     * @return boolean|NULL true / false or NULL if no explicit setting is given
+     */
+    protected function checkAccess($user_id, $model, $reference_id, $reference_id_name) {
         $bAllowAccess = NULL;
 
         // fetch all group assignments for the given user
         $groupItems = Yii::app()->authManager->getAuthItems(2, $user_id);
         
         foreach( $groupItems as $groupName => $groupItem ) {
-            $groupAccess = $this->botanicalObjectAccessGroup($groupName, $botanical_object_id);
+            $groupAccess = $this->checkAccessByType($model, $reference_id, $reference_id_name, $groupName, "group");
             
             // if no entry exists, continue
-            if( $groupAccess == NULL ) continue;
+            if( $groupAccess == "" ) continue;
             
             // if access is allowed, assign & stop
-            if( $groupAccess == true ) {
+            if( $groupAccess == 1 ) {
                 $bAllowAccess = true;
                 break;
             }
@@ -38,66 +68,51 @@ class Authorization extends CComponent {
         }
         
         // check user level access
-        $userAccess = $this->botanicalObjectAccessUser($user_id, $botanical_object_id);
+        $userAccess = $this->checkAccessByType($model, $reference_id, $reference_id_name, $user_id, "user");
         if( $userAccess != NULL ) {
-            $bAllowAccess = $userAccess;
+            $bAllowAccess = ($userAccess == 1) ? true : false;
         }
         
         // finally return access specification
         return $bAllowAccess;
     }
-    
+
     /**
-     * Checks for group access on botanical object level
-     * @param string $group Name of group to check for
-     * @param int $botanical_object_id Botanical Object to check access for
-     * @return boolean|NULL
+     * Generic function for checking the access level
+     * @param CActiveRecord $model model to use for checking
+     * @param int $reference_id ID of reference to check (e.g. 1)
+     * @param string $reference_id_name Name of reference to check (e.g. botanical_object_id)
+     * @param int $access_id ID of access to check (e.g. 5)
+     * @param string $access_type either "user" or "group"
+     * @return string|int returns empty string on no value given, 0 on denied 1 on granted
      */
-    public function botanicalObjectAccessGroup($group, $botanical_object_id) {
-        // check if group has an assignment in the access table
-        $model_accessBotanicalObject = AccessBotanicalObject::model()->findByAttributes(
+    protected function checkAccessByType($model, $reference_id, $reference_id_name, $access_id, $access_type = "user") {
+        switch($access_type) {
+            case 'group':
+                $access_type_name = 'AuthItem_name';
+                break;
+            case 'user':
+                $access_type_name = 'user_id';
+            default:
+                break;
+        }
+        
+        $model_access = $model->findByAttributes(
                 array(
-                    'AuthItem_name' => $group,
-                    'botanical_object_id' => $botanical_object_id
+                    $access_type_name => $access_id,
+                    $reference_id_name => $reference_id
                 )
         );
         
         // if no entry exists, return default value
-        if( $model_accessBotanicalObject == NULL ) return "";
+        if( $model_access == NULL ) return "";
         
         // check value of entry and return permission for it
-        if( $model_accessBotanicalObject->allowDeny == 1 ) {
+        if( $model_access->allowDeny == 1 ) {
             return 1;
         }
         else {
             return 0;
         }
-    }
-    
-    /**
-     * Check for user access on botanical object level
-     * @param int $user_id ID of user to check for
-     * @param int $botanical_object_id ID of botanical object to check
-     * @return boolean|NULL
-     */
-    public function botanicalObjectAccessUser($user_id, $botanical_object_id) {
-        // try to find entry for user
-        $model_accessBotanicalObject = AccessBotanicalObject::model()->findByAttributes(
-                array(
-                    'user_id' => $user_id,
-                    'botanical_object_id' => $botanical_object_id
-                )
-        );
-        
-        // if no entry exists, return default value
-        if( $model_accessBotanicalObject == NULL ) return "";
-        
-        // check value of entry and return permission for it
-        if( $model_accessBotanicalObject->allowDeny == 1 ) {
-            return 1;
-        }
-        else {
-            return 0;
-        }
-    }
+    }    
 }
