3e32c707ed67025348b2e477157b94ba8bbc08fc
wkollernhm <wolfgang.koller@nhm-wien.ac.at>
2014-03-29 20:29:22 +0100

    synonym and scientific name field are now using 80% of the available space (#0000172)

--- protected/views/livingPlant/form_basicTab.php
+++ protected/views/livingPlant/form_basicTab.php
@@ -1,6 +1,6 @@
 <!-- scientific name -->
 <div class="row">
-    <table border="0" width="100%">
+    <table width="100%">
         <tr>
             <td>
                 <?php echo $form->labelEx($model_botanicalObject, 'scientific_name_id'); ?>
@@ -19,6 +19,9 @@
                             }',
                     ),
                     'value' => $model_botanicalObject->scientificName,
+                    'htmlOptions' => array(
+                        'style' => 'width: 80%;'
+                    ),
                 ));
                 ?>
                 <a href="#" onclick="$('#scientific_name_information_dialog').dialog('open'); return false;"><img src="images/page_white_edit.png" ></a>
@@ -122,7 +125,7 @@ if( Yii::app()->user->checkAccess('oprtn_assignLabelType') || Yii::app()->user->
 ?>
 <!-- marking for label printing -->
 <div class="row">
-    <table style="width: auto;">
+    <table style="width: 100%;">
         <tr>
             <td>
                 <?php echo $form->labelEx(LabelType::model(), 'label_type_id'); ?>
@@ -150,6 +153,9 @@ if( Yii::app()->user->checkAccess('oprtn_assignLabelType') || Yii::app()->user->
                             }',
                     ),
                     'value' => $model_livingPlant->labelSynonymScientificName,
+                    'htmlOptions' => array(
+                        'style' => 'width: 80%;'
+                    ),
                 ));
                 ?>
                 <?php echo $form->hiddenField($model_livingPlant, 'label_synonym_scientific_name_id'); ?>
