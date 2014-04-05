1ea54cf54cace764e9da40532f3e7ee08973e480
viras <viras@users.sourceforge.net>
2014-04-05 20:41:23 +0200

    updating deploy-sql files for latest release

--- docs/deploy/changes.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE `tbl_living_plant` ADD `label_annotation` TEXT NULL ;
-
--- Update stored procedure in legacy code:
--- GetScientificName
--- _buildScientificName
--- _buildScientificNameComponents
