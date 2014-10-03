b82081d281bcac67213ec5b0cc3a88a0e02b8161
viras <viras@users.sourceforge.net>
2014-10-03 20:15:50 +0200

    adapting data-model of index seminum for compliance with requirements

--- docs/deploy/changes.sql
+++ docs/deploy/changes.sql
@@ -2,8 +2,6 @@
 -- -----------------------------------------------------
 -- Table `tbl_index_seminum_revision`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `tbl_index_seminum_revision` ;
-
 CREATE TABLE IF NOT EXISTS `tbl_index_seminum_revision` (
   `index_seminum_revision_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `user_id` INT NOT NULL,
@@ -17,20 +15,26 @@ CREATE TABLE IF NOT EXISTS `tbl_index_seminum_revision` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-
 -- -----------------------------------------------------
 -- Table `tbl_index_seminum_content`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `tbl_index_seminum_content` ;
-
 CREATE TABLE IF NOT EXISTS `tbl_index_seminum_content` (
   `index_seminum_content_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `index_seminum_revision_id` INT UNSIGNED NOT NULL,
   `botanical_object_id` INT NOT NULL,
   `accession_number` INT NOT NULL,
+  `family` TEXT NOT NULL,
   `scientific_name` TEXT NOT NULL,
   `index_seminum_type` VARCHAR(3) NOT NULL,
   `ipen_number` VARCHAR(20) NOT NULL,
+  `acquisition_number` TEXT NULL,
+  `acquisition_locality` VARCHAR(45) NULL,
+  `habitat` VARCHAR(45) NULL,
+  `altitude_min` INT NULL,
+  `altitude_max` INT NULL,
+  `longitude` VARCHAR(45) NULL,
+  `latitude` VARCHAR(45) NULL,
+  `acquisition_date` VARCHAR(45) NULL,
   `timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`index_seminum_content_id`),
   INDEX `fk_tbl_index_seminum_content_tbl_botanical_object1_idx` (`botanical_object_id` ASC),
@@ -43,6 +47,23 @@ CREATE TABLE IF NOT EXISTS `tbl_index_seminum_content` (
   CONSTRAINT `fk_tbl_index_seminum_content_tbl_index_seminum_revision1`
     FOREIGN KEY (`index_seminum_revision_id`)
     REFERENCES `tbl_index_seminum_revision` (`index_seminum_revision_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `tbl_index_seminum_person`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tbl_index_seminum_person` (
+  `index_seminum_person_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `index_seminum_content_id` INT UNSIGNED NOT NULL,
+  `name` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`index_seminum_person_id`),
+  INDEX `fk_tbl_index_seminum_person_tbl_index_seminum_content_idx` (`index_seminum_content_id` ASC),
+  CONSTRAINT `fk_tbl_index_seminum_person_tbl_index_seminum_content`
+    FOREIGN KEY (`index_seminum_content_id`)
+    REFERENCES `tbl_index_seminum_content` (`index_seminum_content_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
