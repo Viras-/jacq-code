d4ef1e67d238755c4343cac67f9018c998eb5a62
viras <viras@users.sourceforge.net>
2014-11-03 21:29:59 +0100

    accessControl filter is now really enabled for all controllers

--- protected/controllers/AutoCompleteController.php
+++ protected/controllers/AutoCompleteController.php
@@ -221,15 +221,6 @@ class AutoCompleteController extends JSONServiceController {
     }
 
     /**
-     * @return array action filters
-     */
-    public function filters() {
-        return parent::filters() + array(
-            'accessControl', // perform access control for CRUD operations
-        );
-    }
-
-    /**
      * Specifies the access control rules.
      * This method is used by the 'accessControl' filter.
      * @return array access control rules
