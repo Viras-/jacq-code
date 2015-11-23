91d27b804dc0f328dafaae973512412f82280e43
viras <viras@users.sourceforge.net>
2015-11-23 20:22:53 +0100

    copy & new issues - acquisition data - gardening data - collection management - fixing custom collection date to be saved correctly

--- protected/views/livingPlant/form_relevancy.php
+++ protected/views/livingPlant/form_relevancy.php
@@ -4,13 +4,11 @@
     <?php
     // Find all checked relevancy types for this entry
     $selected_relevancyTypes = array();
-    if (!$model_livingPlant->isNewRecord) {
-        $models_relevancy = Relevancy::model()->findAll('living_plant_id=:living_plant_id', array(':living_plant_id' => $model_livingPlant->id));
+    $models_relevancy = $model_livingPlant->relevancies;
 
-        // Add all selected relevancy types to array
-        foreach ($models_relevancy as $model_relevancy) {
-            $selected_relevancyTypes[] = $model_relevancy->relevancy_type_id;
-        }
+    // Add all selected relevancy types to array
+    foreach ($models_relevancy as $model_relevancy) {
+        $selected_relevancyTypes[] = $model_relevancy->relevancy_type_id;
     }
 
     // Create checkbox list for all relevancy type entries
