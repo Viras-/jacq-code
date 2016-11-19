387f04006fc0a4df90a7e14a107ee988fbea2c53
viras <viras@users.sourceforge.net>
2016-11-19 12:45:54 +0100

    adapting image derivatives processing for new image servlet (public/private marking)

--- protected/views/livingPlant/form_derivativesTab.php
+++ protected/views/livingPlant/form_derivativesTab.php
@@ -18,14 +18,22 @@
             // construct new image server instance
             $jacqImageServer = new JacqImageServer($imageServer->base_url, $imageServer->key);
 
-            $resources = $jacqImageServer->listResources(array(
+            $resourcesDetails = $jacqImageServer->listResources(array(
                 $model_livingPlant->accessionNumber . '_%'
             ));
 
-            foreach ($resources as $resource) {
+            $resources = [];
+            foreach ($resourcesDetails as $resourceDetails) {
+                $resources[] = $resourceDetails->identifier;
+            }
+            $resources = join(',', $resources);
+
+            foreach ($resourcesDetails as $resourceDetails) {
                 ?>
-                <a href="<?php echo $imageServer->base_url; ?>/jacq-viewer/viewer.html?rft_id=<?php echo $resource; ?>&identifiers=<?php echo join(',', $resources); ?>" target="_blank">
-                    <img width="160" src="<?php echo $imageServer->base_url; ?>/adore-djatoka/resolver?url_ver=Z39.88-2004&rft_id=<?php echo $resource ?>&svc_id=info:lanl-repo/svc/getRegion&svc_val_fmt=info:ofi/fmt:kev:mtx:jpeg2000&svc.format=image/jpeg&svc.scale=160,0" />
+                <a href="<?php echo $imageServer->base_url; ?>/jacq-viewer/viewer.html?rft_id=<?php echo $resourceDetails->identifier; ?>&identifiers=<?php echo $resources; ?>" target="_blank">
+                    <img width="160" src="<?php echo $imageServer->base_url; ?>/adore-djatoka/resolver?url_ver=Z39.88-2004&rft_id=<?php echo $resourceDetails->identifier ?>&svc_id=info:lanl-repo/svc/getRegion&svc_val_fmt=info:ofi/fmt:kev:mtx:jpeg2000&svc.format=image/jpeg&svc.scale=160,0" />
+                    <br />
+                    <?php echo Html::checkBox("resource_" + $resourceDetails->identifier, ($resourceDetails->public == 0) ? false : true); ?>
                 </a>
                 &nbsp;
                 <?php
